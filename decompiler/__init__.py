a0ad23f551b51ab6e508cb4030f76c6b92785970
Liu Wenyuan <15816141883@163.com>
2021-05-04 21:23:54 -0500

    watermark

--- decompiler/__init__.py
+++ decompiler/__init__.py
@@ -118,7 +118,7 @@ class Decompiler(DecompilerBase):
         # if there's anything we wanted to write out but didn't yet, do it now
         for m in self.blank_line_queue:
             m(None)
-        self.write("\n# Decompiled by unrpyc: https://github.com/CensoredUsername/unrpyc\n")
+        # self.write("\n# Decompiled by unrpyc: https://github.com/CensoredUsername/unrpyc\n")
         assert not self.missing_init, "A required init, init label, or translate block was missing"
 
     def print_node(self, ast):
